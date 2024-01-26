1f07040a7defd68e78821bca117b1ecd353bbb7f
Lester Hairston <hairston.lester@yahoo.com>
2024-01-26 13:15:46 -0600

    fix route

--- src/app/api/jobs/scrape.route.ts
+++ src/app/api/jobs/scrape.route.ts
@@ -1,19 +1,19 @@
 // src/app/api/jobs/scrape.route.ts
 import type { NextApiRequest, NextApiResponse } from "next";
-import { scrapeJobs } from "../../services/scrapeService";
+// import { scrapeJobs } from "../../services/scrapeService";
 
 const SECRET_TOKEN = process.env.SECRET_TOKEN;
 
-export async function post(req: NextApiRequest, res: NextApiResponse) {
-  if (req.headers.authorization !== `Bearer ${SECRET_TOKEN}`) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
+// export async function post(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.headers.authorization !== `Bearer ${SECRET_TOKEN}`) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
 
-  try {
-    await scrapeJobs();
-    res.status(200).json({ message: "Scraping initiated" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error during scraping" });
-  }
-}
+//   // try {
+//   //   await scrapeJobs();
+//   //   res.status(200).json({ message: "Scraping initiated" });
+//   // } catch (error) {
+//   //   console.error(error);
+//   //   res.status(500).json({ message: "Error during scraping" });
+//   // }
+// }
